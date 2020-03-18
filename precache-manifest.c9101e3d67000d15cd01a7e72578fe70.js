3ecea90187a464150daa9da28221608da13d0bfa
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-03-18 16:11:07 +0000

    Deploy sahithvibudhi/sync-beat to github.com/sahithvibudhi/sync-beat.git:gh-pages

---
+++ precache-manifest.c9101e3d67000d15cd01a7e72578fe70.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "64c07598fb8a8048646762b3767ce14f",
+    "url": "/sync-beat/index.html"
+  },
+  {
+    "revision": "62d76fbf1bd67a747ea0",
+    "url": "/sync-beat/static/css/2.4b6e3ef2.chunk.css"
+  },
+  {
+    "revision": "e61a714ea3694f46e177",
+    "url": "/sync-beat/static/css/main.0b4f85fd.chunk.css"
+  },
+  {
+    "revision": "62d76fbf1bd67a747ea0",
+    "url": "/sync-beat/static/js/2.7246b77d.chunk.js"
+  },
+  {
+    "revision": "eff3b4d7e4bb0087ce53139cd90bc880",
+    "url": "/sync-beat/static/js/2.7246b77d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e61a714ea3694f46e177",
+    "url": "/sync-beat/static/js/main.830e05ed.chunk.js"
+  },
+  {
+    "revision": "55d591513cc873ffef60",
+    "url": "/sync-beat/static/js/runtime-main.998a6af2.js"
+  }
+]);
